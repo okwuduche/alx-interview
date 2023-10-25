f0cd63e591ecd5d1b64f4e489faa649ffe51f48c
okwuduche <chinweikeokwuduche@gmail.com>
2023-10-25 10:38:37 +0000

    fixed error

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,52 +1,40 @@
 #!/usr/bin/python3
-"""
-Define validUTF8(data) function that validates whether a
-string of ints represents a valid UTF-8 encoding.
-"""
-from itertools import takewhile
+"""UTF-8 Validation"""
+
+def get_significant_bits(num):
+    """get significant bits"""
+    set_bits = 0
+    one_two_eight = 128
+    while one_two_eight & num:
+        set_bits += 1
+        one_two_eight = one_two_eight >> 1
+    return set_bits
 
 
-def int_to_bits(nums):
+def check_continuation_bytes(num_set_bits, index, data):
     """
-    Helper function
-    Convert ints to bits
+    Check if the continuation bytes of a multi-byte UTF-8 sequence are valid,
+    given the number of set bits.
     """
-    for num in nums:
-        bits = []
-        mask = 1 << 8  # cause we have 8 bits per byte. adds up to (11111111)
-        while mask:
-            mask >>= 1
-            bits.append(bool(num & mask))
-        yield bits
+    for i in range(index + 1, index + num_set_bits):
+        if i >= len(data):
+            return False
+        if (data[i] & 0b11000000) != 0b10000000:
+            return False
+    return True
 
 
 def validUTF8(data):
-    """
-    Takes a list of ints and returns true if the list is
-    a valid UTF-8 encoding, else returns false
-    Args:
-        data : List of ints representing possible UTF-8 encoding
-    Return:
-        bool : True or False
-    """
-    bits = int_to_bits(data)
-    for byte in bits:
-        # if single byte char, then valid. continue
-        if byte[0] == 0:
-            continue
-
-        # if here, byte is multi-byte char
-        ones = sum(takewhile(bool, byte))
-        if ones <= 1:
+    """check valid utf8"""
+    i = 0
+    while i < len(data):
+        num_set_bits = get_significant_bits(data[i])
+        if num_set_bits == 1 or num_set_bits > 4:
             return False
-        if ones >= 4:  # UTF-8 can be 1 to 4 bytes long
+        if not check_continuation_bytes(num_set_bits, i, data):
             return False
-
-        for _ in range(ones - 1):
-            try:
-                byte = next(bits)
-            except StopIteration:
-                return False
-            if byte[0:2] != [1, 0]:
-                return False
+        if num_set_bits == 0:
+            i += 1
+        else:
+            i += num_set_bits
     return True
